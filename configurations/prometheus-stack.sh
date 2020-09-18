ede8dbfaec1e693dda9b81c2eb982d6fc725bfb2
Hitokiri Mata <hitokiri-mata@users.noreply.github.com>
2020-09-17 23:35:05 -0500

    Update prometheus-stack.sh

--- configurations/prometheus-stack.sh
+++ configurations/prometheus-stack.sh
@@ -3,9 +3,9 @@ set -ex
 
 # These steps follow the readme here: https://github.com/coreos/prometheus-operator/tree/master/helm
 # With RBAC disabled
-//helm repo add coreos https://s3-eu-west-1.amazonaws.com/coreos-charts/stable/
+#helm repo add coreos https://s3-eu-west-1.amazonaws.com/coreos-charts/stable/
 
-//https://github.com/hitokiri-mata/helm-charts/tree/main/charts
+#https://github.com/hitokiri-mata/helm-charts/tree/main/charts
 
 helm repo add coreos https://charts.bitnami.com/bitnami
 helm install coreos/prometheus-operator --name prometheus-operator --namespace monitoring
