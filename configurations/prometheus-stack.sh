266ce7393a1b54402710505007e6cbee1162308d
Hitokiri Mata <hitokiri-mata@users.noreply.github.com>
2020-09-18 21:21:09 -0500

    Update prometheus-stack.sh

--- configurations/prometheus-stack.sh
+++ configurations/prometheus-stack.sh
@@ -13,16 +13,16 @@ set -ex
 #helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo add stable https://kubernetes-charts.storage.googleapis.com/
 #helm repo update
-
-helm install prometheus-operator stable/prometheus-operator --namespace=monitoring
-helm install kube-prometheus stable/kube-prometheus --namespace=monitoring --wait
+#helm install stable/prometheus --set name=prom -f k8s-values.yml
+helm install prometheus-operator stable/prometheus-operator
+helm install kube-prometheus stable/kube-prometheus --wait
 
 #helm  install prometheus-community/kube-prometheus
 #helm upgrade --install --namespace monitoring --set rbacEnable=false prometheus-operator helm/prometheus-operator  
 #helm upgrade --install --namespace monitoring --set rbacEnable=false kube-prometheus coreos/kube-prometheus --wait
 
-kubectl patch service kube-prometheus               --namespace=monitoring --type='json' -p='[{"op": "replace",  "path": "/spec/type", "value":"NodePort"}]'
-kubectl patch service kube-prometheus-alertmanager  --namespace=monitoring --type='json' -p='[{"op": "replace",  "path": "/spec/type", "value":"NodePort"}]'
-kubectl patch service kube-prometheus-grafana       --namespace=monitoring --type='json' -p='[{"op": "replace",  "path": "/spec/type", "value":"NodePort"}]'
+kubectl patch service kube-prometheus               --type='json' -p='[{"op": "replace",  "path": "/spec/type", "value":"NodePort"}]'
+kubectl patch service kube-prometheus-alertmanager  --type='json' -p='[{"op": "replace",  "path": "/spec/type", "value":"NodePort"}]'
+kubectl patch service kube-prometheus-grafana       --type='json' -p='[{"op": "replace",  "path": "/spec/type", "value":"NodePort"}]'
 
 minikube service list -n monitoring
